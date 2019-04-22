fb7da7cf543ba84b3162ca9cfb53238c35378b5a
tmxxx17 <31364829+tmxxx17@users.noreply.github.com>
2019-04-22 22:27:44 +0600

    dfsdf
    dsfsd

---
+++ doxy-support-ticket/includes/admin/views/wpas-help-and-support.php
@@ -0,0 +1,34 @@
+<div class="wrap about-wrap">
+
+	<h1><?php esc_html_e( 'Help and Support', 'awesome-support' ); ?></h1>
+
+	<div class="about-text" style="margin-bottom:0px;"><?php esc_html_e( 'Thanks for using Doxy Awesome Support', 'awesome-support' ); ?></div>
+
+	<div class="changelog">
+
+		<div class="row" style="margin-top: 0px;">
+			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6" style="margin-top: 0px;">
+				<div class="about-body">
+					<h2 style="margin-top: 0px;"><?php esc_attr_e( 'Email Us', 'awesome-support' ); ?></h2>
+					<p><?php esc_attr_e( 'For any support issue please send us email at support@themeix.com or if you have any general query feel free email at themeix@gmail.com', 'awesome-support' ); ?></p>
+					<p><?php echo sprintf( __( '<a href="%s">Contact Us</a>', 'awesome-support' ), 'https://themeix.com/contact/', 'target="_blank"' ) ; ?></p>
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6" style="margin-top: 0px;">
+				<div class="about-body">
+					<h2 style="margin-top: 0px;"><?php esc_attr_e( 'Create Ticket', 'awesome-support' ); ?></h2>
+					<p><?php esc_attr_e( 'Just create a ticket in our support portal, we wil get bacck to you as soon as possible', 'awesome-support' ); ?></p>
+					<p><?php echo sprintf( __( '<a href="%s">Join Themeix Support Portal</a>', 'awesome-support' ), 'https://support.themeix.com', 'target="_blank"' ) ; ?></p>
+				</div>
+			</div>
+		</div>
+
+		<h2><?php esc_html_e( 'Do not hessitate to contact us for help', 'awesome-support' ); ?></h2>		
+		
+		<p><a href="https://themeix.com/"> <img src="https://raw.githubusercontent.com/themeix/static-banners/master/thmeix-doxy-promo-banner.png" alt="Doxy Promo banner"></a></p>
+
+		
+		
+	</div>
+
+</div>
